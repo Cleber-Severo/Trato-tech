443bbb002810f39844fe60d6f339fae3ba2effe9
Cléber Severo <clebersevero1998@gmail.com>
2023-09-01 10:36:19 -0300

    feat: navbar items and icons from react icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import styles from  './Navbar.module.scss'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { RiShoppingCartFill, RiShoppingCart2Line } from 'react-icons/ri';
+import classNames from 'classnames'
+
+const iconeProps =  {
+  color: 'white',
+  size: 24
+}
 
 const Navbar = () => {
   return (
-        <nav className={styles.nav}>
-          <Logo className={styles.logo} />
-        </nav>
-  )
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a
+            href="/"
+            className={classNames(styles.link, {
+              [styles.selected]: window.location.pathname === "/",
+            })}
+          >
+            Página inicial
+          </a>
+        </div>
+      </div>
+      <div className={styles.busca}></div>
+      <div className={styles.icones}>
+        <a href="/carrinho">
+          {window.location.pathname === "/carrinho" 
+            ? <RiShoppingCartFill {...iconeProps} />
+            : <RiShoppingCart2Line {...iconeProps} />
+          }
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
