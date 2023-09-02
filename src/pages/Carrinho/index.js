7fdc6ba45154bf5621d12b81e8ff992383b98780
Cléber Severo <clebersevero1998@gmail.com>
2023-09-02 18:47:17 -0300

    chore: adding itens to the cart with a function that mixes cart list and itens info list

---
+++ src/pages/Carrinho/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './Carrinho.module.scss'
+import Header from 'components/Header'
+import { useSelector } from 'react-redux'
+import Item from 'components/Item'
+
+function Carrinho() {
+    const carrinho = useSelector( state => {
+        const carrinhoReduce = state.carrinho.reduce((itens, itemNoCarrinho) => {
+            const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
+            itens.push({
+                ...item,
+                quantidade: itemNoCarrinho.quantidade
+            });
+            return itens;
+          },[]);
+          return carrinhoReduce; 
+    })
+  return (
+    <div>
+        <Header 
+            titulo='Carrinho de compras'
+            descricao='Confira os produtos que você acidionou no carrinho'
+        />
+        <div className={styles.carrinho}>
+            {carrinho.map( item => <Item key={item.id} {...item} />)}
+            <div className={styles.total}>
+                <strong>
+                    Resumo da compra
+                </strong>
+                <span>
+                    Subtotal: <strong> R$ {0.0.toFixed(2)} </strong>
+                </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Carrinho
