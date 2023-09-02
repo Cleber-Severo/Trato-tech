47c273f35783bff31c059ba3025ebb33a60186a6
Cléber Severo <clebersevero1998@gmail.com>
2023-09-02 20:23:08 -0300

    feat: filter by search with regex and matchin itens title on categories page and cart page

--- src/components/Busca/index.js
+++ src/components/Busca/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Busca.module.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { mudarBusca, resetarBusca } from 'store/reducers/busca'
+import { useLocation } from 'react-router-dom'
+
 
 const Busca = () => {
+  const busca = useSelector(state => state.busca)
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(resetarBusca())
+  }, [location.pathname, dispatch])
+
   return (
     <div className={styles.busca}>
-        <input type="text" className={styles.input} placeholder='O que voce procura ?'/>
+        <input 
+          type="text" 
+          className={styles.input} 
+          placeholder='O que voce procura ?'
+          value={busca}
+          onChange={ evento => dispatch(mudarBusca(evento.target.value))}
+        />
     </div>
   )
 }
