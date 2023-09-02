d8f18ecfe550087e257af5254a5d06786fbba036
Cléber Severo <clebersevero1998@gmail.com>
2023-09-02 17:54:08 -0300

    refactor: Navbar component now using react-router-dom

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import styles from  './Navbar.module.scss'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
-import { RiShoppingCartFill, RiShoppingCart2Line } from 'react-icons/ri';
-import classNames from 'classnames'
-import Busca from 'components/Busca';
+import React from "react";
+import styles from "./Navbar.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { RiShoppingCartFill, RiShoppingCart2Line } from "react-icons/ri";
+import classNames from "classnames";
+import Busca from "components/Busca";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
-const iconeProps =  {
-  color: 'white',
-  size: 24
-}
+const iconeProps = {
+  color: "white",
+  size: 24,
+};
 
 const Navbar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.nav}>
-      <Logo className={styles.logo} />
+      <Logo className={styles.logo} onClick={() => navigate("/")} />
       <div className={styles.links}>
         <div>
-          <a
-            href="/"
+          <Link
+            to="/"
             className={classNames(styles.link, {
-              [styles.selected]: window.location.pathname === "/",
+              [styles.selected]: location.pathname === "/",
             })}
           >
             Página inicial
-          </a>
+          </Link>
         </div>
       </div>
       <div className={styles.busca}>
         <Busca />
       </div>
       <div className={styles.icones}>
-        <a href="/carrinho">
-          {window.location.pathname === "/carrinho" 
-            ? <RiShoppingCartFill {...iconeProps} />
-            : <RiShoppingCart2Line {...iconeProps} />
-          }
-        </a>
+        <Link to="/carrinho">
+          {location.pathname === "/carrinho" ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}
+        </Link>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
