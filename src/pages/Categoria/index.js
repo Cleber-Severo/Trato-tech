aba874b5fdccfeb8c8e7d4dd6b0ee34d0c4e9739
Cléber Severo <clebersevero1998@gmail.com>
2023-09-02 17:06:27 -0300

    feat: items iteration logic

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,17 +1,33 @@
 import Header from "components/Header";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styles from './Categoria.module.scss'
 
 export default function Categoria() {
-    const { nomeCategoria } = useParams()
-    const categoria = useSelector( state => state.categorias.find( categoria => categoria.id === nomeCategoria));
-    return(
-        <div>
-            <Header 
-                titulo={categoria.nome}
-                descricao={categoria.descricao}
-                imagem={categoria.header}
-            />
+  const { nomeCategoria } = useParams();
+  const { categoria, itens } = useSelector((state) => ({
+    categoria: state.categorias.find((categoria) => categoria.id === nomeCategoria),
+    itens: state.itens.filter((item) => item.categoria === nomeCategoria),
+  }));
+        
+  
+  console.log(itens);
+  return (
+    <div>
+      <Header
+        titulo={categoria.nome}
+        descricao={categoria.descricao}
+        imagem={categoria.header}
+      />
+
+        <div className={styles.items} >
+            {itens?.map( item => (
+                <div key={item.id}>
+                    {item.titulo}
+                </div>
+            ))}
         </div>
-    )
+
+    </div>
+  );
 }
